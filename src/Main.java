9d1dc4f5af49f279605118b83601acc57370ad4a
Valeria Locurcio <v.locurcio@hotmail.com>
2026-01-15 20:11:56 +0100

    All files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public  class Main {
+    public static void main(String[] args) {
+
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        String dataString = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+        System.out.println("Data: " + dataString);
+    }
+}
